e1f56d7fe18af8ea3467eb2aa06b6abd314e35f2
Olaf Gladis <olaf@pokermania.de>
2015-07-14 17:25:54 +0200

    added autojump

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,21 @@
+set -g PATH ~/bin/ $PATH
+
+# TODO: nvm-fish
+
+keychain --quiet --agents "ssh,gpg" --nogui --dir ~/.ssh/.keychain ~/.ssh/id_rsa ~/.ssh/id_ed25519
+if test -f ~/.ssh/.keychain/"$HOSTNAME-sh"
+    source ~/.ssh/.keychain/"$HOSTNAME-sh"
+end
+if test -f ~/.ssh/.keychain/"$HOSTNAME-sh-gpg"
+    source ~/.ssh/.keychain/"$HOSTNAME-sh-gpg"
+end
+if test -f ~/.autojump/share/autojump/autojump.fish
+    source ~/.autojump/share/autojump/autojump.fish
+end
+alias p "python3"
+alias p2 "python2"
+alias p3 "python3"
+alias v "v3"
 alias v "v3"
 alias vs "v3s"
 alias a "activate"
