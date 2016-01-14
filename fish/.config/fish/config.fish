2538d19e561e8e1122bdca93d61ddac922faa0ff
Olaf Gladis <olaf@pokermania.de>
2016-01-14 16:37:31 +0100

    fish should always say we use an 256 color term, vim should look nice

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -31,3 +31,5 @@ set -xU LESS_TERMCAP_se (printf "\e[0m")          # end standout-mode
 set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
 set -xU LESS_TERMCAP_ue (printf "\e[0m")          # end underline
 set -xU LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
+
+set -xU TERM xterm-256color
