50a69d5599225c20569773a3e720f0599a94c0dc
Olaf Gladis <olaf@gladis.org>
2017-08-04 19:42:03 +0200

    add better venvs for bash

--- fish/.config/fish/functions/activate.fish
+++ fish/.config/fish/functions/activate.fish
@@ -3,16 +3,15 @@ function activate --description "looking for virtual envs to activate"
     while test "$PWD" != "/"
         if test -f ./.env3/bin/activate
             . .env3/bin/activate.fish
-            popd > /dev/null
-            return
+            break
         else
             if test -f ./.env2/bin/activate
-            . .env2/bin/activate.fish
-                popd > /dev/null
-                return
+                . .env2/bin/activate.fish
+                break
             else
                 cd ..
             end
         end
     end
+    popd > /dev/null
 end
