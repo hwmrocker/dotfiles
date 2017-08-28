13621982f35f026ccda6283f021d23471f59de31
Olaf Gladis <olaf.gladis@trivago.com>
2017-08-28 15:54:51 +0200

    ...

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -19,6 +19,7 @@ if status --is-interactive
       source ~/.autojump/share/autojump/autojump.fish
   end
 end
+
 alias p "python"
 alias p2 "python2"
 alias p3 "python3"
@@ -31,12 +32,12 @@ alias md "mkdir -p"
 alias rd "rmdir"
 alias vi "vim"
 
-set -xU LESS_TERMCAP_mb (printf "\e[01;31m")      # begin blinking
-set -xU LESS_TERMCAP_md (printf "\e[01;31m")      # begin bold
-set -xU LESS_TERMCAP_me (printf "\e[0m")          # end mode
-set -xU LESS_TERMCAP_se (printf "\e[0m")          # end standout-mode
-set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
-set -xU LESS_TERMCAP_ue (printf "\e[0m")          # end underline
-set -xU LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
+set -xg LESS_TERMCAP_mb (printf "\e[01;31m")      # begin blinking
+set -xg LESS_TERMCAP_md (printf "\e[01;31m")      # begin bold
+set -xg LESS_TERMCAP_me (printf "\e[0m")          # end mode
+set -xg LESS_TERMCAP_se (printf "\e[0m")          # end standout-mode
+set -xg LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
+set -xg LESS_TERMCAP_ue (printf "\e[0m")          # end underline
+set -xg LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
 
-set -xU TERM xterm-256color
+set -xg TERM xterm-256color
